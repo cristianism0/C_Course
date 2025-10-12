9ece4ecd2a17f24b21ec3528a66cc694958bfc39
cristianism0 <csg2099@gmail.com>
2025-10-12 17:03:14 -0300

    cap2: iterations

---
+++ 2.control/merge_quick_sorts.c
@@ -0,0 +1,29 @@
+/*
+    Divide and Conquest:
+    This both sort algoritms is based on a way called "Divide and Conquest", which means:
+    - Divide a problem P in m sub-problems.
+    - Solve them recursively
+    - Get all m solutions to evaluates P.
+*/
+
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main (int argc, [[maybe_unused]] argv) {
+    double A[10] = {12.8, 11.4, 10.0, 26.5, 0.5, 1.5, 3.7, 6.9, 5.8, 45.8};
+
+    // Quick Sort iteration
+    for (size_t i = 0, double A[10]; i <= 10; ++i) {              // i <= because A has 10 elements.
+        
+
+
+    }
+
+
+
+    // Merge Sort iteration
+
+
+    return EXIT_SUCCESS;
+}
